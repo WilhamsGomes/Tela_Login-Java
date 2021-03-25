7dc6b5437f10a7c530ef09592ee1dbd54132cb4e
WilhamsGomes <wilhamsgoomes@gmail.com>
2021-03-25 19:06:08 -0300

    Commit alteração na estrutura

--- src/Telas/Tela_Secundaria.java
+++ src/Telas/Tela_Secundaria.java
@@ -7,44 +7,84 @@ public class Tela_Secundaria extends javax.swing.JFrame {
     public Tela_Secundaria() {
         initComponents();
     }
-
+    
+    public String senha02;
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btn_voltar = new javax.swing.JButton();
-        lbl_logo = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
+        PainelCor = new javax.swing.JPanel();
+        btn_cod = new javax.swing.JButton();
+        lbl_cod = new javax.swing.JLabel();
+        btn_voltar1 = new javax.swing.JButton();
+        txt_cod = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        btn_voltar.setText("Voltar");
-        btn_voltar.addActionListener(new java.awt.event.ActionListener() {
+        PainelCor.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btn_cod.setText("Gerar código");
+        btn_cod.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_voltarActionPerformed(evt);
+                btn_codActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_voltar, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 360, -1, -1));
+        PainelCor.add(btn_cod, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 210, 100, 40));
+        PainelCor.add(lbl_cod, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 50, 180, 50));
 
-        lbl_logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Acesso.png"))); // NOI18N
-        getContentPane().add(lbl_logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(-7, -20, 470, 410));
+        btn_voltar1.setText("Voltar");
+        btn_voltar1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_voltar1ActionPerformed(evt);
+            }
+        });
+        PainelCor.add(btn_voltar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 280, 100, 40));
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 460, 390));
+        txt_cod.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_codActionPerformed(evt);
+            }
+        });
+        PainelCor.add(txt_cod, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 130, 180, 50));
+
+        getContentPane().add(PainelCor, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 460, 390));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btn_voltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_voltarActionPerformed
-     
-        Tela_login obj = new Tela_login();
-        obj.setVisible(true);
-        dispose();
+    public void senha() {
         
-    }//GEN-LAST:event_btn_voltarActionPerformed
+         String[] carct ={"0","1","2","3","4","5","6","7","8","9"};
+         String senha="";
+            for (int x=0; x<4; x++){
+            int j = (int) (Math.random()*carct.length);
+            senha += carct[j];
+            }
+
+            lbl_cod.setText(senha);
+           
+    }
+    
+    private void btn_codActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_codActionPerformed
+
+        senha ();
+        
+        
+    }//GEN-LAST:event_btn_codActionPerformed
+
+    private void btn_voltar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_voltar1ActionPerformed
+       
+        
+       
+        
+    }//GEN-LAST:event_btn_voltar1ActionPerformed
+
+    private void txt_codActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_codActionPerformed
+      
+    }//GEN-LAST:event_txt_codActionPerformed
 
     
     public static void main(String args[]) {
@@ -80,8 +120,10 @@ public class Tela_Secundaria extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_voltar;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel lbl_logo;
+    private javax.swing.JPanel PainelCor;
+    private javax.swing.JButton btn_cod;
+    private javax.swing.JButton btn_voltar1;
+    private javax.swing.JLabel lbl_cod;
+    private javax.swing.JTextField txt_cod;
     // End of variables declaration//GEN-END:variables
 }
